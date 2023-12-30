b91c1fa69c67da5e8abe76cc3d86d5a0d939ff38
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-12-30 17:57:31 -0600

    updating app

--- app.ts
+++ app.ts
@@ -2,14 +2,24 @@ import { isSame, sameFrequency } from "./sameFrequency";
 
 // console.log("App Running");
 
-console.log("Is Same?");
-isSame(182, 281); // true
-isSame(34, 14); // false
-isSame(3589578, 5879385); // true
-isSame(22, 222); // false
+// ##########################################################################################
+/**
+O(n)
+  what is better?
+  - Faster?
+  - Less memory?
+  - More readable?
 
-console.log("Same Frequency");
-console.log(sameFrequency(182, 281)); // true
-console.log(sameFrequency(34, 14)); // false
-console.log(sameFrequency(3589578, 5879385)); // true
-console.log(sameFrequency(22, 222)); // false
+ */
+
+// console.log("Is Same?");
+// isSame(182, 281); // true
+// isSame(34, 14); // false
+// isSame(3589578, 5879385); // true
+// isSame(22, 222); // false
+
+// console.log("Same Frequency");
+// console.log(sameFrequency(182, 281)); // true
+// console.log(sameFrequency(34, 14)); // false
+// console.log(sameFrequency(3589578, 5879385)); // true
+// console.log(sameFrequency(22, 222)); // false
