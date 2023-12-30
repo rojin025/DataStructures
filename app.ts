1ccb87bf9b447d5b2f211fbc4fe606cb5d6978d9
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-12-29 18:48:58 -0600

    Updating with Same Frequency Pattern

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-console.log("App Running");
+// console.log("App Running");
 
 //
 /**
@@ -29,7 +29,39 @@ function rem(num1: number) {
 // // rem(101);
 // rem(0);
 
+console.log("Is Same?");
 isSame(182, 281); // true
 isSame(34, 14); // false
 isSame(3589578, 5879385); // true
 isSame(22, 222); // false
+
+type Count = { [key: string]: number };
+
+function sameFrequency(num1: number, num2: number): boolean {
+  const strNum1 = num1.toString();
+  const strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1: Count = {};
+  let countNum2: Count = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+  }
+
+  for (let j = 0; j < strNum2.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+  }
+
+  for (const key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
+
+console.log("Same Frequency");
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3589578, 5879385)); // true
+console.log(sameFrequency(22, 222)); // false
