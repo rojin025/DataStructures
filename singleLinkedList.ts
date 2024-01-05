c17f1cc2f95e2625b5e2f1dcf9fc60a0d559a637
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-01-04 21:30:25 -0600

    Pop and shift added.

--- singleLinkedList.ts
+++ singleLinkedList.ts
@@ -32,4 +32,32 @@ export class SingleLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    const oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    return oldHead;
+  }
 }
