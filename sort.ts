07e790128beaf9d72b9fca9d28ed7b529a8eb9cb
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-01-11 20:14:06 -0600

    Merge Sort Completed

--- sort.ts
+++ sort.ts
@@ -75,3 +75,40 @@ function insertionSort(arr: number[]): number[] {
 }
 
 // console.log(insertionSort(arrNum));
+
+/** Merge Sort */
+function merge(arr1: number[], arr2: number[]): number[] {
+  let result: number[] = [];
+  let i = 0,
+    j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    console.log(".");
+    console.log((arr1[i], arr2[j]));
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+// console.log(merge([1, 3, 4], [2, 5, 6, 7]));
+// [3,2,1,4]
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+console.log(mergeSort([3, 2, 1, 4]));
