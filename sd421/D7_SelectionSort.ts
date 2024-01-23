1dee28cf62912ab8852e8a2ed618f430d2f09584
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-01-22 21:40:49 -0600

    Assignment 7 - Insertion sort ADT

--- sd421/D7_SelectionSort.ts
+++ sd421/D7_SelectionSort.ts
@@ -11,3 +11,18 @@ repeat (numOfElements - 1) times
         set element as new minimum
 
 */
+
+/*
+Insertion-sort algorithm steps
+
+mark first element as sorted
+
+for each unsorted element X 
+    'extract' the element X
+
+    for j = lastSortedIndex down to 0
+
+        if current element j > X
+            move sorted element to the right by 1
+            break loop and insert X here
+*/
