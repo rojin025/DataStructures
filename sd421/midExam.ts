148fc147b77093e113718efbfe23729fe988848c
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-01-25 21:34:35 -0600

    Stack - Isbalanced pseudo code.

--- sd421/midExam.ts
+++ sd421/midExam.ts
@@ -32,6 +32,23 @@ function sumFirstHelper(num: number, sum: number) {
   return sumFirstHelper(num - 1, num + sum);
 }
 
+export function sum(n: number): number {
+  return sumHelper(1, n);
+}
+
+function sumHelper(start: number, end: number): number {
+  if (start > end) {
+    return 0;
+  } else {
+    const mid = Math.floor((start + end) / 2);
+    const leftSum = sumHelper(start, mid - 1);
+    const rightSum = sumHelper(mid + 1, end);
+    console.log("L: ", leftSum);
+    console.log("R: ", rightSum);
+    return leftSum + rightSum + mid;
+  }
+}
+
 // Multi
 export function fibonacci(num: number): number {
   if (num < 0) return 0;
@@ -58,60 +75,40 @@ export function power(x: number, k: number): number {
   }
 }
 
-export function sum(n: number): number {
-  return sumHelper(1, n);
-}
-
-function sumHelper(start: number, end: number): number {
-  if (start > end) {
-    return 0;
+export function powerLog(x: number, n: number): number {
+  if (n === 0) {
+    return 1;
   } else {
-    const mid = Math.floor((start + end) / 2);
-    const leftSum = sumHelper(start, mid - 1);
-    const rightSum = sumHelper(mid + 1, end);
-    console.log("L: ", leftSum);
-    console.log("R: ", rightSum);
-    return leftSum + rightSum + mid;
+    const partial = powerLog(x, Math.floor(n / 2));
+    let result = partial * partial;
+
+    if (n % 2 === 1) {
+      result *= x;
+    }
+
+    return result;
   }
 }
 
-// FindMax
-
+// Pseudo code of isBalance
 /*
-function findMax(array):
-    // Base case: if array is empty, return null 
-    if array is empty:
-        return null;
+  function isBalanced ( str: string): boolean{
+    Create an empty stack of characters
+    balanced = true;
+    opening = ['(', '[', '{'];
+    closing = [')', ']', '}'];
+    pairs = {'(': ')', '[': ']', '{': '}'}
 
-    // Get the first number of array
-    firstElement = array[0];
-
-    retun maxHelper(arrat, 1 , firstElement);
-
-function maxHelper(array, index, currentMax)    
-  if index = array.length
-    return currentMax
-  else
-    currentMax = Math.max(currentMax, array[index]);
-    index++;
-    return maxHelper(array, index, macurrentM)  
-    */
-
-/*
-Time complexity is O(logn)
-function sum(n):
-    // Base case: if n is 0, return 0
-    if n == 0:
-        return 0
-    else if n == 1:
-        return 1
-    else:
-        // Split the problem into two halves and make two recursive calls
-        mid = Math.floor(n / 2)
-        sumLeft = sum(mid)
-        sumRight = sum(n - mid)
-        
-        // Combine the results of the two halves
-        return sumLeft + sumRight
-
-    */
+    index = 0;
+    currentChar : string;
+    
+    While (balanced && index < str.length) {
+      currentChar = str[index];
+      if (currentChar in opening) stack.push(currentChar)
+      else if (currentChar in closing && (stack.isEmpty() || pairs[stack.pop()] != currentChar )) balanced = false
+      index++;
+    }
+    if (balanced && stack.isEmpty()) return true;
+    return false.
+  }
+*/
