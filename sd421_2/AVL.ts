1fd96af4b756fd334acad4784abc1a452f854a78
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-01-30 19:41:08 -0600

    AVL - RotateLeft

--- sd421_2/AVL.ts
+++ sd421_2/AVL.ts
@@ -14,3 +14,26 @@ Algorithm findValue(key, value)
         key > value.key()        
         return findValue(key, T.rightChild(value))
 
+Algorithm rotateLeft(Tree, Node)
+    Input Binary Tree : Tree and Node
+    Output a left rotation around Node is performed
+
+    if Tree.isRoot(Node) throw InvalidLeftRotation
+    parent = Tree.parent(Node);
+    grandParent = Tree.parent(parent);
+
+    Tree.setRightChild(parent, Tree.leftChild(Node))
+    if Tree.isInternal(Tree.leftChild(Node))
+        Tree.setParent(Tree.leftChild(Node), parent)
+
+    Tree.setLeftChild(Node, parent)    
+    Tree.setParent(parent, Node)
+
+    if Tree.isRoot(parent)
+        Tree.setRoot(Node)
+    else 
+        if Tree.rightChild(grandParent) = parent
+            Tree.setRightChild(grandParent, Node)        
+        else
+            Tree.setLeftChild(grandParent, Node)    
+        Tree.setParent(Node, GrandParent)
