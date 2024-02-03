d938c2444317a4cdccdffa41e15d67aaf5cb5116
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-02 21:30:06 -0600

    D5- DFS psuedoCode Complete.

--- sd421_2/D5_DFS_BFS.ts
+++ sd421_2/D5_DFS_BFS.ts
@@ -3,3 +3,17 @@
 
 Algorithm: Depth First Search
 input : Graph = (V ,E)
+Output :G, with all vertices marked as visited
+
+    Initialize a Stack  stack
+    Pick a starting vertex A and mark it as visited
+    stack.push(A)
+    while( !stack.isEmpty())
+        Vertex = stack.peek()
+        
+        if some vertex adjacent to Vertex is not yet visited then
+            unvisitedVertex = next unvisited vertex adjacent to Vertex
+            mark unvisitedVertex
+            stack.push( unvisitedVertex )
+        else 
+            stack.pop()
