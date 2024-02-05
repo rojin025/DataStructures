26c0bf2f567d183abd6709696b9eed23e970c475
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-04 19:06:03 -0600

    D6_ALgo Freactional Knapsack algo

--- sd421_2/D6_Algos.ts
+++ sd421_2/D6_Algos.ts
@@ -1,7 +1,7 @@
 /*
 
 // DijkstraDistance
-Algorithm DijkstraDistances(Graph , s)
+Algorithm DijkstraDistances(Graph , s)// s = set of keys and element - vertex
     priorityQueue = new PriorityQueue
     for all vertex of Graph.vertices() 
         if vertex = s
@@ -10,4 +10,20 @@ Algorithm DijkstraDistances(Graph , s)
             setDistance (vertex, ∞ )
         priorityQueue.insertItem(getDistance(vertex), vertex);
         while (!prorityQueue.isEmpty())
-                
+            u   // minimal vertex
+        // incidentEdges = all adjacent edges of that vertex
+
+// Fractional Knapsack Algo
+Algorithm fractionalKnapsack(S, W)
+Input: set S of items w/ benefit bi and weight wi; max. weight W
+Output: amount xi of each item i to maximize benefit with weight at most W
+
+for each item i in S 
+    xi = 0
+    vi = bi / wi    // {value}
+    w = 0           //{total weight} 
+
+while w < W do
+    remove item i with highest vi
+    xi = min{wi ,W-w}
+    w  = w + min{wi ,W-w}
