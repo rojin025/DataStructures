555f52d51790888a976a5452915541863692a148
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-02 22:13:50 -0500

    Addin big O

--- ZTM/Arrays/array.ts
+++ ZTM/Arrays/array.ts
@@ -2,5 +2,8 @@ const str = ["a", "b", "c"];
 
 console.log("Output is :", str[2]);
 
-str.push("d");
-str.pop();
+str.push("d"); // O(1)
+str.pop(); // O(1)
+
+str.unshift("0"); // O(n)
+console.log(str);
