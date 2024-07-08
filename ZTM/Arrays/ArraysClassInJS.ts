3a5b92b043845f2265907881271bd732698713e7
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-08 18:20:49 -0500

    Adding Class in JS.

--- ZTM/Arrays/ArraysClassInJS.ts
+++ ZTM/Arrays/ArraysClassInJS.ts
@@ -3,12 +3,29 @@ class Player {
   type: string;
 
   constructor(name: string, type: string) {
-    console.log("Player", name);
+    console.log("Player", this);
     this.name = name;
     this.type = type;
   }
 
   introduce() {
-    console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    console.log(`Hi I am ${this.name}, and my Type is ${this.type}`);
   }
 }
+
+class Wizard extends Player {
+  name: string;
+  type: string;
+
+  constructor(name: string, type: string) {
+    super(name, type);
+    console.log("wizard: ", this);
+  }
+  play() {
+    console.log("We are playing the game, ", this.type);
+  }
+}
+
+const wizard = new Wizard("John", "Healer");
+wizard.introduce();
+wizard.play();
