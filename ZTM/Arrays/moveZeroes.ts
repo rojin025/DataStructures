5612adaefb7a82ced3b8b4d004e197a161a4b304
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-15 17:49:32 -0500

    Updatin solution.

--- ZTM/Arrays/moveZeroes.ts
+++ ZTM/Arrays/moveZeroes.ts
@@ -1,23 +1,45 @@
+console.log("Running");
+
 /**
- Do not return anything, modify nums in-place instead.
+Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-  let updatedNums: number[] = [];
-  let countZeroes = 0;
+  let left = 0;
+  let right = 1;
 
   if (nums.length === 0) return;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) {
-      countZeroes++;
-    } else {
-      updatedNums.push(nums[i]);
+  while (right < nums.length) {
+    if (nums[right] !== 0 && nums[left] === 0) {
+      nums[left] = nums[right];
+      nums[right] = 0;
     }
+    if (nums[left] !== 0) {
+      left++;
+    }
+    right++;
   }
-
-  for (let i = 0; i < countZeroes; i++) {
-    updatedNums.push(0);
-  }
-
-  nums = [...updatedNums];
+  console.log(nums);
 }
+
+// [0,1,0,3,12]
+// [1,0,0,3,12]
+// [1,3,0,0,12]
+// [1,3,12,0,0]
+
+moveZeroes([1, 0, 1]);
+moveZeroes([2, 1]);
+moveZeroes([0, 1, 0, 3, 12]);
+
+/**
+ * 
+ * Good Solution
+  for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            numZeros++
+        } else if (numZeros > 0) {
+            // Shift
+            nums[i - numZeros] = nums[i];
+        }
+    }
+ */
